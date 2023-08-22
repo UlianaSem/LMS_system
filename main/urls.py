524cc740a3ab99d810c35457fb3aaba6c2ffae57
Uliana Semyonova <i@ulianasemyonova.ru>
2023-08-22 20:33:27 +0500

    Update main

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from main.views import home
+from main.views import home, contacts
+from main.apps import MainConfig
+
+app_name = MainConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from main.models import Student
 
 
 def home(request):
-    return render(request, 'main/index.html')
+    context = {
+        'object_list': Student.objects.all(),
+        'title': "Главная"
+    }
+    return render(request, 'main/index.html', context)
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        print(f'Свяжитесь с {name} {email}')
+
+    context = {
+        'object_list': Student.objects.all(),
+        'title': "Наши контакты"
+    }
+    return render(request, 'main/contacts.html', context)
+
