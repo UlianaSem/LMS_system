ca56e652fa95e6acc96283f5e9e490ccd6a57858
Uliana Semyonova <i@ulianasemyonova.ru>
2023-09-05 19:25:31 +0500

    Update urls and view

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import StudentListView, contacts, StudentDetailView, StudentCraeteView, StudentUpdateView, StudentDeleteView, toggle_activity
+from main.views import (StudentListView, contacts, StudentDetailView, StudentCreateView, StudentUpdateView,
+                        StudentDeleteView, toggle_activity)
 from main.apps import MainConfig
 
 app_name = MainConfig.name
@@ -8,7 +9,7 @@ urlpatterns = [
     path('', StudentListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('student/<int:pk>/', StudentDetailView.as_view(), name='student_view'),
-    path('create/', StudentCraeteView.as_view(), name='create_student'),
+    path('create/', StudentCreateView.as_view(), name='create_student'),
     path('edit/<int:pk>/', StudentUpdateView.as_view(), name='update_student'),
     path('delete/<int:pk>/', StudentDeleteView.as_view(), name='delete_student'),
     path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 
-from main.models import Student
+from main.forms import StudentForm, SubjectForm
+from main.models import Student, Subject
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -27,16 +29,43 @@ class StudentDetailView(DetailView):
     model = Student
 
 
-class StudentCraeteView(CreateView):
+class StudentCreateView(CreateView):
     model = Student
-    fields = ('first_name', 'last_name', 'profile_photo',)
+    form_class = StudentForm
     success_url = reverse_lazy('main:home')
 
 
 class StudentUpdateView(UpdateView):
     model = Student
-    fields = ('first_name', 'last_name', 'profile_photo',)
-    success_url = reverse_lazy('main:home')
+    form_class = StudentForm
+
+    def get_success_url(self):
+        return reverse('main:update_student', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        SubjectFormset = inlineformset_factory(Student, Subject, form=SubjectForm, extra=1)
+
+        if self.request.method == 'POST':
+            formset = SubjectFormset(self.request.POST, instance=self.object)
+        else:
+            formset = SubjectFormset(instance=self.object)
+
+        context_data['formset'] = formset
+
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        print(context_data)
+        formset = context_data['formset']
+        self.object = form.save()
+        
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        
+        return super().form_valid(form)
 
 
 class StudentDeleteView(DeleteView):
