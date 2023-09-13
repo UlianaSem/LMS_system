a0d0858145f81f092ea97627420e20aa63da8953
Uliana Semyonova <i@ulianasemyonova.ru>
2023-09-13 21:40:19 +0500

    Add environment variables and caching

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pnd&_=*5-gt+s6ca8_5u5(ub-iq4tsc%a!)za4*edikmlq@ea$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,3 +144,13 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/'
+
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') is True
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://127.0.0.1:6379",
+        }
+    }
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from main.views import (StudentListView, contacts, StudentDetailView, StudentCreateView, StudentUpdateView,
                         StudentDeleteView, toggle_activity)
 from main.apps import MainConfig
@@ -6,7 +8,7 @@ from main.apps import MainConfig
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', StudentListView.as_view(), name='home'),
+    path('', cache_page(60)(StudentListView.as_view()), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('student/<int:pk>/', StudentDetailView.as_view(), name='student_view'),
     path('create/', StudentCreateView.as_view(), name='create_student'),
